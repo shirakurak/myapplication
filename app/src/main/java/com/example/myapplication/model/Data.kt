1f07eb9942315fcb28d6c492e32a9609a24ffd49
shirakurak <surakura@hotmail.co.jp>
2024-01-22 08:29:41 +0900

    add model/data

---
+++ app/src/main/java/com/example/myapplication/model/Data.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.model
+
+import android.widget.ImageView
+
+data class Data (
+    var icon: ImageView? = null,
+    var title: String? = null,
+    var text: String? = null
+)
